67b4fb85276a76519a3e3bf8d7ccda2fe44516ec
Dennis Chan <denkc@denkc.com>
2013-03-25 03:04:20 -0700

    Match tag name to player name style - save all as downcased

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -7,7 +7,17 @@ class Tag < ActiveRecord::Base
   validates :name, presence: true
   validates_uniqueness_of :name
   
+  before_validation :downcase_name
+  
   def self.names
-    Tag.pluck(:name)
+    Tag.pluck(:name).map(&:titleize)
+  end
+  
+  def downcase_name
+    self.name = self.name.downcase if self.name
+  end
+  
+  def display_name
+    name.titleize
   end
 end

9ffc3a4c0c5ce8db66d8eb891c5e23e14e2c3583
Dennis Chan <denkc@denkc.com>
2013-03-25 02:52:52 -0700

    add tag formatter to view helper, css tweaks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,9 +12,17 @@ module ApplicationHelper
 
   def format_game_scores(scores)
     spans = scores.map do |w, l, status|
-      content_tag 'span', "#{w}-#{l}", class: status
+      content_tag 'span', "#{w}-#{l}", class: 'status'
     end.join
 
     raw(spans)
+  end
+  
+  def format_tags(tags)
+    tags = tags.map do |tag|
+      link_to(content_tag('span', tag.name, class: 'tag'), tag_path(tag), class: 'tag-link')
+    end.join
+
+    raw(tags)
   end
 end
